ac1c8fb646b702c55affc1ea6e559416be206a7e
Yinka Asonibare <yinka.asonibare@theiconic.com.au>
2017-01-03 16:47:35 +1100

    + Changed file-conflict array freeing logic to use ref counting

--- src/lib/file-conflict.c
+++ src/lib/file-conflict.c
@@ -25,11 +25,13 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "array.h"
 #include "file-conflict.h"
 #include "debug.h"
 #include "util-string.h"
 
-static bool fi_config_group_free_cb(struct FiList *file);
+static bool fi_conflict_group_free_cb(struct FiList *file);
+static void free_conflict_group_ref(const struct FiRef *ref);
 
 
 struct FiConfGroup *fi_conflict_group_new()
@@ -42,43 +44,156 @@ struct FiConfGroup *fi_conflict_group_new()
     }
     
     grp->files = NULL;
+    grp->common_container = false;
+    grp->ref.count = 1;
+    grp->ref.free  = free_conflict_group_ref;
 
     return grp;
 }
 
-void fi_conflict_group_free(struct FiConfGroup *grp)
+static void free_conflict_group_ref(const struct FiRef *ref)
 {
+    struct FiConfGroup *grp = container_of(ref, struct FiConfGroup, ref);
     if (! grp)
         return;
-
+    
     // Free the list
     fi_list_free(grp->files);
-
-    free(grp);
+    
+    if (! grp->common_container)
+        free(grp);
 }
 
-static bool fi_config_group_free_cb(struct FiList *node)
+void fi_conflict_group_free(struct FiConfGroup *grp)
 {
-    fi_file_destroy(fi_list_data_ptr(node, struct FiFileInfo));
+
+    if (! grp)
+        return;
+    
+    fi_ref_dec(&grp->ref);
+}
+
+static bool fi_conflict_group_free_cb(struct FiList *node)
+{
+    struct FiFileInfo *info = fi_list_data_ptr(node, struct FiFileInfo);
+    fi_ref_dec(&info->ref_count);
 }
 
 void fi_conflict_group_add(struct FiConfGroup *self, struct FiFileInfo *file)
 {
     if (! self || ! file)
         return;
+        
+    fi_ref_inc(&file->ref_count);
+
+    struct FiList *list = fi_list_new(file, fi_conflict_group_free_cb);
 
     if (! fi_list_count(self->files))
-        self->files = fi_list_new(file, fi_config_group_free_cb);
+        self->files = list;
     else 
-        fi_list_append(self->files, fi_list_new(file, fi_config_group_free_cb));
+        fi_list_append(self->files, list);
+
 }
 
 bool fi_conflict_group_has(struct FiConfGroup *self, struct FiFileInfo *file)
 {
-    for (struct FiList *c = fi_list_head(self->files); c; c = c->next) {
-        if ((struct FiFileInfo *)fi_list_data_ptr(c, struct FiList) == file)
+
+    if (! self || ! file)
+        return false;
+
+    struct FiList *c = NULL;
+
+    for (c = self->files; c->prev; c = c->prev); // Move to head
+
+    for (; c; c = c->next) {
+        if ((struct FiFileInfo *)fi_list_data_ptr(c, struct FiFileInfo) == file)
             return true;
     }
 
     return false;
+}
+
+/// Conflict group cast macro
+#define CONF_GRP(c) ((struct FiConfGroup *)(c))
+/**
+ * Copies struct FiConGroup.
+ * 
+ * Used by FiArray or any other quick copying of FiConfGroup
+ * 
+ * @param src
+ * @param dst
+ * @param n
+ * @return 
+ */
+bool fi_conflict_group_copy(void const *src, void *dst, unsigned n)
+{
+    if (! src || ! dst)
+        return false;
+
+
+    
+    struct FiList *head = NULL;
+    // Rewind to beginning
+    for(head = CONF_GRP(src)->files; head->prev; head = head->prev);
+
+    for(struct FiList *cur = head; cur; cur = cur->next) {
+
+        // create new file
+        struct FiFileInfo *file = malloc(sizeof *file);
+        if (file) {
+            fi_file_init(file);
+            file->free_container = true; // Memory should be freed on deallocation
+            // copy the file
+            fi_file_copy(fi_list_data_ptr(cur, struct FiFileInfo), file);
+            // Add file to new group
+            fi_conflict_group_add(CONF_GRP(dst), file);
+            fi_ref_dec(&file->ref_count); // Free resources
+        }
+
+    }
+
+    CONF_GRP(dst)->conflict_type_id = CONF_GRP(src)->conflict_type_id;
+    CONF_GRP(dst)->ref.free         = CONF_GRP(src)->ref.free;
+    fi_ref_inc( &(CONF_GRP(dst)->ref) );
+    
+}
+
+// ------------------------------------------
+// Conflict Group Array
+// ------------------------------------------
+void fi_conflict_array_init(struct FiConflictArray *arr)
+{
+    if (! arr)
+        return;
+    
+    arr->file_groups = fi_array_new(sizeof *(arr->file_groups),
+                                    fi_conflict_group_copy);
+}
+
+void fi_conflict_array_free(struct FiConflictArray *arr)
+{
+    if (! arr || ! arr->file_groups)
+        return;
+
+    struct FiConfGroup *grp;
+    
+    while(grp = fi_array_pop(arr->file_groups, struct FiConfGroup)) {
+        fi_ref_dec(&grp->ref);  
+    }
+    
+    fi_array_free(arr->file_groups);
+}
+
+void fi_conflict_array_add_group(struct FiConflictArray *arr,
+                                 struct FiConfGroup *grp)
+{
+    if (! arr || ! grp)
+        return;
+
+    fi_array_push(arr->file_groups, grp);
+    struct FiConfGroup *last = fi_array_get_ptr_end(arr->file_groups,
+                                                    struct FiConfGroup);
+    // We are going to be self-managing the destruction of the container
+    last->common_container = true;
+
 }
